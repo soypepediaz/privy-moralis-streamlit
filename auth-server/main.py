c54fe37e9b6f2db493e3b8c336b5e703294c01c2
soypepediaz <100941124+soypepediaz@users.noreply.github.com>
2025-12-02 13:47:20 +0100

    Create main.py

---
+++ auth-server/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+import uvicorn
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
